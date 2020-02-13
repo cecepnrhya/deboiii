ec8aa5cd5ee5bbb2e178ae29e00ff5367e6dcf4f
cecepnrhya <46098300+cecepnrhya@users.noreply.github.com>
2020-02-13 07:01:47 +0700

    Add files via upload

---
+++ Write.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
+
+reader = SimpleMFRC522()
+
+try:
+	text = input('New Data:')
+	print("Now Place Your Tag to Write")
+	reader.write(text)
+	print("Written")
+
+finally:
+	GPIO.cleanup()
